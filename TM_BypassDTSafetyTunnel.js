168b03e3c4c7242d3f6e78f38b2dce0719847d77
Duffhause <57455765+duffhause@users.noreply.github.com>
2022-03-08 16:16:52 +0000

    Add files via upload

---
+++ TM_BypassDTSafetyTunnel.js
@@ -0,0 +1,24 @@
+// ==UserScript==
+// @name         Bypass DT safety tunnel
+// @namespace    stonerteam
+// @version      0.1
+// @description  Remove donutteam saftey tunnel
+// @author       The StonerTeam
+// @include        http*://tunnel.donutteam.com*
+// @icon         https://stoner.team/favicon.png
+// @grant        none
+// ==/UserScript==
+
+// Get tunnels paremeters
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
+
+// Get and decode URI
+const url = urlParams.get('url');
+const decodedUrl = decodeURI(url);
+
+//Redirect
+window.location.replace(decodedUrl);
+
+
+
